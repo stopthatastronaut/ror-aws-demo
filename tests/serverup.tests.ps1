dab467be8f072fe69d87572a502f1951b9535373
Jason Brown <stopthatastronaut@gmail.com>
2021-05-25 23:56:03 +1000

    Add one test

--- tests/serverup.tests.ps1
+++ tests/serverup.tests.ps1
@@ -1,5 +1,13 @@
-﻿Describe "The server should be up" {
+﻿$DNSTarget = "ror-aws-demo.takofukku.io"
+
+Describe "The server should be up" {
     It "Should respond on Port 80" {
-        Invoke-WebRequest -uri http://ror-aws-demo.takofukku.io/ | Select-Object -expand Content | Should -Be "<h1>hello world</h1>"
+        Invoke-WebRequest -uri http://$DNSTarget/ | Select-Object -expand Content | Should -Be "<h1>hello world</h1>"
     }
+
+    It "Should reject port 3389" {
+        new-object System.Net.Sockets.TcpClient("$DNSTarget", 3389)  | Should -Be $null
+    }
+
+
 }
